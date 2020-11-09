bba664353b1bbb77aca7288ce9870aaa3d03e99c
Coreyander <45904565+Coreyander@users.noreply.github.com>
2020-11-09 15:37:52 -0500

    Create Careers.js
    commit for Careers.js
    -added background data field

---
+++ Careers.js
@@ -0,0 +1,101 @@
+import { Images } from "./Images";
+
+export const Careers = [
+  {
+    title: "Zoologist",
+    buttonImg: Images.lion.image,
+    background: Images.zoology_background.image,
+    slug: "zoologist",
+    images: [
+      
+      {
+        title: "lion",
+        image: Images.lion.image,
+        alternate: "Lion",
+        title: "Lion",
+        content:
+          "Zoologists study many different types of animals including lions",
+      },
+    ],
+  },
+  {
+    title: "Marine Biologist",
+    buttonImg: Images.lion.image,
+    background: Images.marine_background.image,
+    slug: "marine-biologist",
+    images: [
+      {
+        title: "lion",
+        image: Images.lion.image,
+        alternate: "Lion",
+        title: "Lion",
+        content:
+          "Zoologists study many different types of animals including lions",
+      },
+      {
+        title: "lion",
+        image: Images.lion.image,
+        alternate: "Lion",
+        title: "Lion",
+        content:
+          "Zoologists study many different types of animals including lions",
+      },
+      {
+        title: "lion",
+        image: Images.lion.image,
+        alternate: "Lion",
+        title: "Lion",
+        content:
+          "Zoologists study many different types of animals including lions",
+      },
+      {
+        title: "lion",
+        image: Images.lion.image,
+        alternate: "Lion",
+        title: "Lion",
+        content:
+          "Zoologists study many different types of animals including lions",
+      },
+    ],
+  },
+  {
+    title: "Archaeologist",
+    buttonImg: Images.lion.image,
+    background: Images.arch_background.image,
+    slug: "archeologist",
+    images: [
+      {
+        title: "lion",
+        image: Images.lion.image,
+        alternate: "Lion",
+        title: "Lion",
+        content:
+          "Zoologists study many different types of animals including lions",
+      },
+      {
+        title: "lion",
+        image: Images.lion.image,
+        alternate: "Lion",
+        title: "Lion",
+        content:
+          "Zoologists study many different types of animals including lions",
+      },
+      {
+        title: "lion",
+        image: Images.lion.image,
+        alternate: "Lion",
+        title: "Lion",
+        content:
+          "Zoologists study many different types of animals including lions",
+      },
+      {
+        title: "lion",
+        image: Images.lion.image,
+        alternate: "Lion",
+        title: "Lion",
+        content:
+          "Zoologists study many different types of animals including lions",
+      },
+    ],
+  },
+];
