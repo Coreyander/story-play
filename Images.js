5b65d5f7b45eb660ff8f8600c85e68d0c0a2aaef
Coreyander <45904565+Coreyander@users.noreply.github.com>
2020-11-09 15:39:49 -0500

    Create Images.js
    -added background images

---
+++ Images.js
@@ -0,0 +1,12 @@
+import lion from "../images/lion.png";
+import zoology_background from "../images/zoology_background.jpg";
+import marine_background from "../images/marine_background.jpg";
+import arch_background from "../images/arch_background.jpg";
+
+export const Images = {
+  lion: { image: lion, alternate: "Lion" },
+  lion2: { image: lion, alternate: "Lion" },
+  zoology_background: {image: zoology_background, alternate: "zoo_bg"},
+  marine_background: {image: marine_background, alternate: "marine_bg"},
+  arch_background: {image: arch_background, alternate: "arch_bg"},
+};
